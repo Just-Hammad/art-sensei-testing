9fca858465ab6101bd7b837fecae182c74f42c88
Just-Hammad <hammadarif564@gmail.com>
2025-12-09 02:10:10 +0500

    updated testing userids

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,2 @@
-// export const TEST_USER_ID = 'TESTING_AGENT_APP-TEST_USER_ID';
-export const TEST_USER_ID = `2afd8ab9-302a-4d2f-a272-7f50b344b358`
-// export const TEST_SESSION_ID = `TESTING_AGENT_APP-TEST_SESSION_ID`;
-export const TEST_SESSION_ID = `7c827988-7d27-49f8-b5c7-2e42a9bec668`
-
-// [Chat Store] Resuming session: {sessionId: '7c827988-7d27-49f8-b5c7-2e42a9bec668', userId: '2afd8ab9-302a-4d2f-a272-7f50b344b358'}
+export const TEST_USER_ID = 'TESTING_AGENT_APP-TEST_USER_ID';
+export const TEST_SESSION_ID = `TESTING_AGENT_APP-TEST_SESSION_ID`;
